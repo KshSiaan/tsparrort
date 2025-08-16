a6f7a011f4710aabd0437e31fd26af4604d69e75
Ksh Siaan <kshsiaan@gmail.com>
2025-08-16 14:23:40 +0600

    quick

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
-
+import { Analytics } from "@vercel/analytics/next";
 export const metadata: Metadata = {
   title: "The Screaming Parrots Cafe – Desserts, Tea & Bites in Redmond, WA",
   description:
@@ -46,6 +46,7 @@ export default function RootLayout({
           disableTransitionOnChange
         >
           {children}
+          <Analytics />
         </ThemeProvider>
       </body>
     </html>
