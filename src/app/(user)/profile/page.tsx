a5e3bc5f53fa722b5c0d3942fcda5975118946f6
Ksh Siaan <kshsiaan@gmail.com>
2025-10-05 16:38:10 +0600

    quick

--- src/app/(user)/profile/page.tsx
+++ src/app/(user)/profile/page.tsx
@@ -3,17 +3,7 @@ import React from "react";
 import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Mail, Phone } from "lucide-react";
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
-import { items } from "@/lib/products";
-import { Badge } from "@/components/ui/badge";
-import Link from "next/link";
+
 import { useQuery } from "@tanstack/react-query";
 import { getProfileApi } from "@/lib/api/auth";
 import { useCookies } from "react-cookie";
